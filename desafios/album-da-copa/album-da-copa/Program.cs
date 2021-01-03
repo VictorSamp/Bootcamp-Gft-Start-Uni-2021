93ddbd22c857fef00dd767076805b59669e3adce
Victor Sampaio <victor.msampaio@outlook.com.br>
2021-01-03 16:14:41 -0300

    Adicionando pasta desafios no bootcamp

---
+++ desafios/album-da-copa/album-da-copa/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace album_da_copa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeFigurinhas, figurinhasFaltando;
+        }
+    }
+}
---
+++ desafios/calculo-de-viagem/calculo-de-viagem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculo_de_viagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input;
+            int tempoGasto, velocidadeMedia;
+            double totalLitros;
+
+            input = Console.ReadLine().Split(" ");
+            tempoGasto = int.Parse(input[0]);
+            velocidadeMedia = int.Parse(input[1]);
+
+            totalLitros = tempoGasto * velocidadeMedia / 12.00;
+
+            Console.WriteLine(totalLitros.ToString("N3", CultureInfo.InvariantCulture));
+        }
+    }
+}
